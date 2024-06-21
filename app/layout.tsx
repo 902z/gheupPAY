8b18360f92da85ffbe95f0f8e0504f2545dd078b
김보미 <kimbomi2172@naver.com>
2024-06-21 19:01:06 +0900

    💄 ui: reset.css 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import '@/styles/globals.css'
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+import "@/styles/reset.css";
 
 export const metadata: Metadata = {
-  title: '급PAY',
-  description: '급하게 일손이 필요한 자리에 더 많은 시급을 제공해서 아르바이트생을 구할 수 있는 서비스',
-}
+  title: "급PAY",
+  description: "급하게 일손이 필요한 자리에 더 많은 시급을 제공해서 아르바이트생을 구할 수 있는 서비스",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="ko">
       <body>{children}</body>
     </html>
-  )
+  );
 }
-
