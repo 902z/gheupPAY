88212c15959e56b2a75c362e16404f73011ba64a
김보미 <kimbomi2172@naver.com>
2024-06-21 12:13:20 +0900

    ✨ feat: 테일윈드 색상 변수 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        black: "#111322",
+        gray: {
+          50: "#7D7986",
+          40: "#A4A1AA",
+          30: "#CBC9CF",
+          20: "#E5E4E7",
+          10: "#F2F2F3",
+          5: "#FAFAFA",
+        },
+        red: {
+          40: "#FF4040",
+          30: "#FF8D72",
+          20: "#FFAF9B",
+          10: "#FFEBE7",
+        },
+        blue: {
+          20: "#0080FF",
+          10: "#CCE6FF",
+        },
+        green: {
+          20: "#20A81E",
+          10: "#D4F7D4",
+        },
+        kakao: "#FEE500",
+      },
     },
   },
   plugins: [],
