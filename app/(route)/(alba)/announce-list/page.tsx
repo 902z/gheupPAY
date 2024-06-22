7ef626563926a9f7bf7628500bb70b538a3442bb
codefug <86544979+codefug@users.noreply.github.com>
2024-06-22 16:53:27 +0900

    ✨ feat: title 메타데이터 동적 기능 추가

--- app/(route)/(alba)/announce-list/page.tsx
+++ app/(route)/(alba)/announce-list/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+export const metadata = {
+  title: "공고 리스트",
+};
 export default function page() {
   return <div>announce-list</div>;
 }
--- app/(route)/admin/store-edit/page.tsx
+++ app/(route)/admin/store-edit/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+export const metadata = {
+  title: "가게 정보 편집",
+};
 export default function page() {
   return <div>stroe-edit</div>;
 }
