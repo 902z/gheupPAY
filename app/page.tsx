f0f16922ac17c072c0f4dab6a3abe05170fd572a
김보미 <kimbomi2172@naver.com>
2024-06-22 10:49:07 +0900

    📦 chore: 폰트 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold">메인페이지</h1>
-      <p>아아아아아아아아</p>
-      <p>안녕하세요</p>
+      <h1 className="font-bold">폰트를 굵게 하고 싶으면 font-bold를 쓰세요.</h1>
+      <p>기본은 regular입니다. 따로 설정 안 해도 돼요.</p>
     </>
   );
 }
