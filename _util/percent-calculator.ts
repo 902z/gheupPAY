846e898e338f1a4fc6b02ea45d201b59fa14b90b
김보미 <kimbomi2172@naver.com>
2024-06-22 17:09:29 +0900

    ✨ feat: 시급퍼센트에 따른 라벨 컴포넌트 구현

---
+++ _util/percent-calculator.ts
@@ -0,0 +1,3 @@
+export const percentCalculator = (hourlyRate: number) => {
+  return Math.round((9860 / hourlyRate) * 100);
+};
--- app/(route)/(alba)/announce-list/page.tsx
+++ app/(route)/(alba)/announce-list/page.tsx
@@ -1,5 +1,13 @@
+import { percentCalculator } from "@/_util/percent-calculator";
+import LabelHourlyRate from "@/app/components/label-hourly-rate";
 import React from "react";
 
 export default function page() {
-  return <div>announce-list</div>;
+  const priceExample = 15000;
+  return (
+    <div>
+      announce-list
+      <LabelHourlyRate percent={percentCalculator(priceExample)} />
+    </div>
+  );
 }
