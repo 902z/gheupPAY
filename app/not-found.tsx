a9fa046e1db29edb382c3cb07b111d431fd3126c
codefug <86544979+codefug@users.noreply.github.com>
2024-06-22 16:54:51 +0900

    ✨ feat: not-found.tsx 추가 및 메타데이터 추가

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "잘못된 경로입니다",
+};
+export default function NotFound() {
+  return (
+    <>
+      <div className="text-center">
+        <p>에러 페이지</p>
+      </div>
+    </>
+  );
+}
