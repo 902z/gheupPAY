748f0c32f024c095821769e09f8246e16ca128e7
codefug <86544979+codefug@users.noreply.github.com>
2024-06-22 17:46:44 +0900

    📦 chore: 전체 페이지 컨테이너 컴포넌트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <main className="mx-3 mb-[126px] mt-[102px] md:mx-8 md:mb-[100px] md:mt-[70px] lg:mx-auto lg:max-w-[964px]">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
