ba89f0bc605c4893479e16a8782ac789d3221aff
hongseoha <seoseo.ha.h@gmail.com>
2024-06-23 21:34:36 +0900

    :lipstick: ui: 상세 필터 컴포넌트 디자인 적용

--- app/(route)/(alba)/announce-list/_components/filter/index.tsx
+++ app/(route)/(alba)/announce-list/_components/filter/index.tsx
@@ -2,10 +2,6 @@
 
 import React from "react";
 import { useState } from "react";
-import Image from "next/image";
-
-import close from "@/public/icons/close.png";
-import closeRed40 from "@/public/icons/close-red-40.png";
 
 const addressList = [
   "서울시 종로구",
@@ -47,38 +43,66 @@ export default function Filter() {
   };
   return (
     <>
-      <div>
-        <h2>상세 필터</h2>
-        <Image src={close} alt="닫기" width={24} height={24} />
-      </div>
-      <h3>위치</h3>
-      <div>
-        {/* 주소 목록 */}
-        {addressList.map((address) => (
-          <div key={address} onClick={() => handleAddressClick(address)}>
-            {address}
+      <div className="ml-[20px] w-[390px] border border-black px-[20px] py-[24px]">
+        <div className="mb-[24px] flex justify-between font-bold text-l">
+          <h2>상세 필터</h2>
+          <div className="cursor-pointer">×</div>
+        </div>
+        <div className="w-[350px] border-b pb-[24px]">
+          <h3>위치</h3>
+          <div className="my-[12px] grid h-[258px] w-[350px] grid-cols-2 overflow-scroll rounded-[6px] border border-gray-20 px-[20px] px-[28px] text-m">
+            {addressList.map((address) => (
+              <div
+                className="mt-[20px] w-[94px] cursor-pointer"
+                key={address}
+                onClick={() => handleAddressClick(address)}
+              >
+                {address}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div>
-        {/* 선택된 주소 목록 */}
-        {selectedAddresses.map((address) => (
-          <div key={address}>{address}</div>
-        ))}
-        <div>
-          <p>서울시 강남구</p>
-          <Image src={closeRed40} alt="닫기" width={16} height={16} />
+          <div>
+            <div className="flex flex-wrap gap-[8px]">
+              {selectedAddresses.map((address) => (
+                <div
+                  className="flex justify-between gap-[4px] rounded-[20px] bg-red-10 px-[10px] px-[7px] py-[6px] text-m text-red-40"
+                  key={address}
+                >
+                  {address}
+                  <div className="cursor-pointer">×</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="mt-[24px] h-[92px] w-[350px]">
+          <p className="mb-[8px]">시작일</p>
+          <input
+            className="h-[58px] w-[350px] rounded-[6px] border border-gray-30 px-[20px] py-[16px]"
+            placeholder="입력"
+          ></input>
+        </div>
+        <div className="mt-[24px] h-[92px] w-[350px] border-t-2 border-gray-10">
+          <p className="mb-[8px] mt-[24px]">금액</p>
+          <div className="relative flex">
+            <input
+              className="h-[58px] w-[169px] rounded-[6px] border border-gray-30 px-[20px] py-[16px]"
+              placeholder="입력"
+            ></input>
+            <p className="mx-[12px] my-[16px]">이상부터</p>
+            <p className="absolute left-[134px] top-[16px]">원</p>
+          </div>
+        </div>
+        {/* 공통 컴포넌트- 버튼 넣을 자리 */}
+        <div className="mt-[56px] flex h-[48px] gap-[8px] border">
+          <div className="h-[48px] w-[82px] rounded-[6px] border-2 border-red-40 pt-[12px] text-center font-bold text-red-40">
+            초기화
+          </div>
+          <div className="h-[48px] w-[260px] rounded-[6px] border bg-red-40 pt-[14px] text-center font-bold text-white">
+            적용하기
+          </div>
         </div>
       </div>
-      <div>
-        <p>시작일</p>
-        <input placeholder='입력'></input>
-      </div>
-      <div>
-        <p>금액</p>
-        <input placeholder='입력'></input> 이상부터
-      </div>
-      <div>여기 버튼 두개 넣는 자리</div>
     </>
   );
 }
--- app/(route)/(alba)/announce-list/page.tsx
+++ app/(route)/(alba)/announce-list/page.tsx
@@ -4,6 +4,8 @@ import Filter from './_components/filter';
 export default function page() {
   return <>
   <div>announce-list</div>
+  
+  {/* 컴포넌트 확인용 */}
   <Filter></Filter>
   </>
 }
