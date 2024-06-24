162cb25468bf2313aba05d39ae4b7840f40ef2cd
김보미 <kimbomi2172@naver.com>
2024-06-24 12:26:28 +0900

    ♻️ refactor: 퍼센트 계산기 로직 수정

--- _util/calculate-wage-percentage .ts
+++ _util/calculate-wage-percentage .ts
@@ -12,5 +12,5 @@ export const calculateWagePercentage = (hourlyWage: number) => {
   if (hourlyWage < MINIMUN_HOURLY_WAGE) {
     throw new Error("최저시급보다 낮게 금액을 설정할 수 없습니다.");
   }
-  return Math.round((MINIMUN_HOURLY_WAGE / hourlyWage) * 100);
+  return Math.round(((hourlyWage - MINIMUN_HOURLY_WAGE) / MINIMUN_HOURLY_WAGE) * 100);
 };
