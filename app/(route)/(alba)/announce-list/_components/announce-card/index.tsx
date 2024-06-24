2aaa48331c60ceedcf6ef3c0b735803524e4cd9e
김보미 <kimbomi2172@naver.com>
2024-06-24 11:19:04 +0900

    ♻️ refactor: 공고 카드 컴포넌트 수정

--- app/(route)/(alba)/announce-list/_components/announce-card/index.tsx
+++ app/(route)/(alba)/announce-list/_components/announce-card/index.tsx
@@ -3,8 +3,12 @@ import React from "react";
 import clock from "@/public/icons/clock.png";
 import mapPin from "@/public/icons/map-pin.png";
 import arrowUpRed40 from "@/public/icons/arrow-up-red-40.png";
+import { calculateWagePercentage } from "@/_util/calculate-wage-percentage ";
+import LabelHourlyRate from "@/app/components/label-hourly-rate";
 
 export default function AnnounceCard() {
+  const priceExample = 15000;
+
   return (
     <article className="h-[261px] w-[171px] rounded-[12px] border border-gray-20 bg-white p-3 md:h-[349px] md:w-[313px]">
       <div className="h-[82px] w-[147px] rounded-[12px] md:h-[160px] md:w-[280px]">이미지</div>
@@ -24,9 +28,9 @@ export default function AnnounceCard() {
         <data className="py-2 text-s text-gray-50 md:text-m">서울시 강남구</data>
       </div>
       <div className="flex flex-col md:flex-row md:gap-4 md:py-4">
-        <data className="pt-1 font-bold text-ml md:text-xl">10,000원</data>
+        <data className="text-ml pt-1 font-bold md:text-xl">10,000원</data>
         <div className="flex items-center text-s text-primary">
-          기존 시급보다 50%
+          <LabelHourlyRate percent={calculateWagePercentage(priceExample)} />
           <div className="h-4 w-4">
             <Image src={arrowUpRed40} alt="up" />
           </div>
--- app/(route)/(alba)/announce-list/page.tsx
+++ app/(route)/(alba)/announce-list/page.tsx
@@ -1,23 +1,11 @@
-import { calculateWagePercentage } from "@/_util/calculate-wage-percentage ";
-import LabelHourlyRate from "@/app/components/label-hourly-rate";
 import React from "react";
 import AnnounceCard from "./_components/announce-card";
 
-/**
- * 주어진 시급이 최저시급 대비 몇 퍼센트인지를 계산합니다.
- *
- * @param {number} hourlyWage - 계산하려는 시급
- * @returns {number} 최저시급 대비 퍼센트 값 (반올림됨)
- * @throws {Error} 주어진 시급이 최저시급보다 낮을 때 에러를 발생시킴
- */
-
 export default function page() {
-  const priceExample = 15000;
   return (
     <div>
       <h2 className="font-bold text-2xl">맞춤 공고</h2>
       <AnnounceCard />
-      <LabelHourlyRate percent={calculateWagePercentage(priceExample)} />
       <h2 className="font-bold text-2xl">전체 공고</h2>
     </div>
   );
