7643101d75c30bbfd8dc7c05f051ef99c19493c7
김보미 <kimbomi2172@naver.com>
2024-06-24 20:41:54 +0900

    ✨ feat: get api 연동

--- app/(route)/(alba)/announce-list/page.tsx
+++ app/(route)/(alba)/announce-list/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AnnounceCard from "./_components/announce-card";
+import getNotices from "@/app/_apis/api";
 
-export default function page() {
+export default async function page() {
+  const data = await getNotices({});
+
   return (
     <div>
       <h2 className="font-bold text-2xl">맞춤 공고</h2>
-      <AnnounceCard />
+      <AnnounceCard notices={data} />
       <h2 className="font-bold text-2xl">전체 공고</h2>
     </div>
   );
---
+++ app/_apis/api.ts
@@ -0,0 +1,30 @@
+import instance from "../_lib/axios";
+
+export default async function getNotices({
+  offset = 0,
+  limit = 10,
+  // address = "",
+  // keyword = "",
+  // startsAtGte = "",
+  // hourlyPayGte = 0,
+  // sort = "pay",
+}) {
+  try {
+    const params = new URLSearchParams({
+      offset: offset.toString(),
+      limit: limit.toString(),
+      // address,
+      // keyword,
+      // startsAtGte,
+      // hourlyPayGte: hourlyPayGte.toString(),
+      // sort,
+    });
+
+    const res = await instance.get(`/notices?${params.toString()}`);
+    console.log(res.data.items);
+    return res.data.items;
+  } catch (error) {
+    console.error("getNotices 함수에서 오류 발생:", error);
+    throw error;
+  }
+}
