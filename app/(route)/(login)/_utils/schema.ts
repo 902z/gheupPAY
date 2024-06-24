9c18de240d722ba2d94f79a5bffebd636307e3af
CHAE <cowhdals1111@gmail.com>
2024-06-25 01:26:23 +0900

    ✨  feat: 스키마 조건 변경

--- app/(route)/(login)/_utils/schema.ts
+++ app/(route)/(login)/_utils/schema.ts
@@ -6,15 +6,20 @@ export const loginSchema = yup.object().shape({
   email: yup
     .string()
     .required(loginErrorMessages.EMAIL_REQUIRED)
-    .matches(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/, loginErrorMessages.INVALID_EMAIL),
+    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, loginErrorMessages.INVALID_EMAIL),
   password: yup.string().required(loginErrorMessages.PASSWORD_REQUIRED).min(8, loginErrorMessages.INVALID_PASSWORD),
 });
 export const signUpSchema = yup.object().shape({
   email: yup
     .string()
     .required(loginErrorMessages.EMAIL_REQUIRED)
-    .matches(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/, loginErrorMessages.INVALID_EMAIL),
-  password: yup.string().required(loginErrorMessages.PASSWORD_REQUIRED).min(8, loginErrorMessages.INVALID_PASSWORD),
+    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, loginErrorMessages.INVALID_EMAIL),
+  password: yup
+    .string()
+    .required(loginErrorMessages.PASSWORD_REQUIRED)
+    .min(8, loginErrorMessages.INVALID_PASSWORD)
+    .matches(/[A-Za-z]/, loginErrorMessages.INVALID_PASSWORD)
+    .matches(/[0-9]/, loginErrorMessages.INVALID_PASSWORD),
   passwordConfirm: yup
     .string()
     .required(loginErrorMessages.CONFIRM_PASSWORD_REQUIRED)
--- app/_constants/validation-text.ts
+++ app/_constants/validation-text.ts
@@ -1,11 +1,8 @@
-
-
 export const loginErrorMessages = {
-    EMAIL_REQUIRED: "이메일을 입력해주세요.",
-    INVALID_EMAIL: "이메일 형식으로 작성해 주세요.",
-    PASSWORD_REQUIRED: "비밀번호를 입력해주세요.",
-    INVALID_PASSWORD: "8자 이상 입력해주세요",
-    CONFIRM_PASSWORD_REQUIRED: "비밀번호 확인란을 입력해주세요.",
-    PASSWORDS_MUST_MATCH: "비밀번호가 일치하지 않습니다.",
-  };
-  
+  EMAIL_REQUIRED: "이메일을 입력해주세요.",
+  INVALID_EMAIL: "이메일 형식으로 작성해 주세요.",
+  PASSWORD_REQUIRED: "비밀번호를 입력해주세요.",
+  INVALID_PASSWORD: "8자 이상 영문, 숫자 조합을 입력해주세요",
+  CONFIRM_PASSWORD_REQUIRED: "비밀번호 확인란을 입력해주세요.",
+  PASSWORDS_MUST_MATCH: "비밀번호가 일치하지 않습니다.",
+};
