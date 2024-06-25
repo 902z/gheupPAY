135ee47f01d331a4c36880685b82830fe8a0fd8f
BORI <88658551+Kbomi16@users.noreply.github.com>
2024-06-25 19:13:53 +0900

    ✨ feat: 공고 리스트 페이지 구현 (#45)
    * ✨ feat: axios instance 생성
    
    * ♻️ refactor: Image 태그 크기 설정
    
    * ✨ feat: get api 연동
    
    * ♻️ refactor: 데이터 수정
    
    * ♻️ refactor: prettier 재설정
    
    * ♻️ refactor: 맞춤 공고용 limit 수정
    
    * ✨ feat: 날짜 포매터 함수 구현
    
    * ✨ feat: 숫자 콤마 함수 구현
    
    * 📝 docs: author추가
    
    * 💄 ui: 카드 호버 추가
    
    * 💄 ui: framer-motion 적용
    
    * ♻️ refacotr: layout 최상단 container 변수 분리
    
    * 💄 ui: 공고 리스트 레이아웃 설정
    
    * 📦 chore: swiper 설치
    
    * 🐛 fix: 이미지 url 설정
    
    * ✨ feat: api 재설정 및 카드 분리
    
    * ♻️ refactor: 카드 반응형 수정
    
    * ♻️ refactor: 스와이퍼 크기에 따른 카드 수정
    
    * ♻️ refactor: 전체 공고 카드 겹치는 현상 해결
    
    * ♻️ refactor: 전체 공고 grid 재설정
    
    * ♻️ refactor: 카드 안 겹치게 설정
    
    * ♻️ refactor: 원 추가
    
    * ♻️ refacotr: 폰트 크기 수정
    
    * 🐛 fix: MINIMUM_HOURLY_WAGE 수정
    
    * ♻️ refactor: 스와이퍼 패딩 설정
    
    * 📝 docs: number-format 주석 추가

--- app/_util/calculate-wage-percentage .ts
+++ app/_util/calculate-wage-percentage .ts
@@ -6,11 +6,14 @@ import { MINIMUM_HOURLY_WAGE } from "../_constants/minimum-hourly-wage";
  * @param {number} hourlyWage - 계산하려는 시급
  * @returns {number} 최저시급 대비 퍼센트 값 (반올림됨)
  * @throws {Error} 주어진 시급이 최저시급보다 낮을 때 에러를 발생시킴
+ * @author 김보미
  */
 
 export const calculateWagePercentage = (hourlyWage: number) => {
   if (hourlyWage < MINIMUM_HOURLY_WAGE) {
     throw new Error("최저시급보다 낮게 금액을 설정할 수 없습니다.");
   }
-  return Math.round(((hourlyWage - MINIMUM_HOURLY_WAGE) / MINIMUM_HOURLY_WAGE) * 100);
+  return Math.round(
+    ((hourlyWage - MINIMUM_HOURLY_WAGE) / MINIMUM_HOURLY_WAGE) * 100,
+  );
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "bootcamp-project-api.s3.ap-northeast-2.amazonaws.com",
+      "i.pinimg.com",
+      "google.com",
+    ],
+  },
+};
 
 export default nextConfig;
