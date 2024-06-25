7d6a24f07d9dc5601998c2b918af9a8bf93b8bc8
codefug <86544979+codefug@users.noreply.github.com>
2024-06-25 19:52:32 +0900

    📦 chore: 랜딩 페이지에 링크 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <div className="text-center">
-        <h1 className="font-bold">폰트를 굵게 하고 싶으면 font-bold를 쓰세요.</h1>
+      <div className="base-container text-center">
+        <Link href="/announce-list">리스트 페이지로 이동</Link>
+        <h1 className="font-bold">
+          폰트를 굵게 하고 싶으면 font-bold를 쓰세요.
+        </h1>
         <p>기본은 regular입니다. (기본은 설정 X)</p>
         <p className="text-s">s: 12px</p>
         <p className="text-m">m: 14px</p>
