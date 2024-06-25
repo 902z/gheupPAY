d58a69d62b9a0a285cf7d49c116c88cba98ed3d2
codefug <86544979+codefug@users.noreply.github.com>
2024-06-25 21:53:32 +0900

    🐛 fix: footer 반응형 수정

--- app/_components/footer/index.tsx
+++ app/_components/footer/index.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      <footer className="relative left-0 right-0 grid grid-cols-2 grid-rows-2 justify-between gap-y-10 bg-gray-10 px-5 pb-4 pt-8 text-gray-50 md:hidden">
-        <p className="flex gap-[30px]">
-          <span>Privacy Policy</span>
+      <footer className="relative left-0 right-0 grid grid-cols-2 grid-rows-2 justify-between gap-y-[34px] bg-gray-10 px-5 pb-4 pt-8 text-gray-50">
+        <p className="flex gap-[30px] text-m font-normal text-gray-50">
+          <span className="text-nowrap">Privacy Policy</span>
           <span>FAQ</span>
         </p>
         <nav className="flex justify-end gap-[10px]">
@@ -15,17 +15,27 @@ export default function Footer() {
             <Image src="/icons/mail.png" alt="mail" width={25} height={25} />
           </Link>
           <Link href="https://www.facebook.com/">
-            <Image src="/icons/facebook.png" alt="facebook" width={25} height={25} />
+            <Image
+              src="/icons/facebook.png"
+              alt="facebook"
+              width={25}
+              height={25}
+            />
           </Link>
           <Link href="https://www.instagram.com/">
-            <Image src="/icons/instagram.png" alt="instagram" width={25} height={25} />
+            <Image
+              src="/icons/instagram.png"
+              alt="instagram"
+              width={25}
+              height={25}
+            />
           </Link>
         </nav>
-        <p>
-          ©<span className="company-name">codeit</span> - 2023
+        <p className="self-end text-s font-normal text-gray-50">
+          ©codeit - 2023
         </p>
       </footer>
-      <footer className="relative left-0 right-0 hidden bg-gray-10 text-gray-50 md:block">
+      {/* <footer className="relative left-0 right-0 hidden bg-gray-10 text-gray-50 md:block">
         <section className="max-w-[964px] justify-between md:flex md:px-8 md:py-[37px] lg:mx-auto">
           <p>
             ©<span className="company-name">codeit</span> - 2023
@@ -34,20 +44,29 @@ export default function Footer() {
             <span>Privacy Policy</span>
             <span>FAQ</span>
           </p>
-
           <nav className="flex justify-end gap-[10px]">
             <Link href="https://mail.google.com/">
               <Image src="/icons/mail.png" alt="mail" width={25} height={25} />
             </Link>
             <Link href="https://www.facebook.com/">
-              <Image src="/icons/facebook.png" alt="facebook" width={25} height={25} />
+              <Image
+                src="/icons/facebook.png"
+                alt="facebook"
+                width={25}
+                height={25}
+              />
             </Link>
             <Link href="https://www.instagram.com/">
-              <Image src="/icons/instagram.png" alt="instagram" width={25} height={25} />
+              <Image
+                src="/icons/instagram.png"
+                alt="instagram"
+                width={25}
+                height={25}
+              />
             </Link>
           </nav>
         </section>
-      </footer>
+      </footer> */}
     </>
   );
 }
