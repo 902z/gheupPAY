25ac48470e019d6b296e2066156ad37986365042
김보미 <kimbomi2172@naver.com>
2024-06-25 20:17:41 +0900

    📝 docs: jsdoc 추가

--- app/_components/modal/index.tsx
+++ app/_components/modal/index.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
+/**
+ * ModalProps 타입 정의
+ * @typedef {Object} ModalProps
+ * @property {boolean} isOpen - 모달이 열려 있는지 여부를 나타내는 상태
+ * @property {() => void} closeModal - 모달을 닫기 위한 함수
+ * @property {React.ReactNode} children - 모달 내부에 렌더링할 콘텐츠
+ * @author 김보미
+ */
+
 type ModalProps = {
   isOpen: boolean;
   closeModal: () => void;
@@ -17,8 +26,9 @@ export default function Modal({ isOpen, closeModal, children }: ModalProps) {
           exit={{ opacity: 0 }}
           transition={{ duration: 0.3 }}
           className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-          onClick={closeModal}
+          onClick={closeModal} // 밖에 클릭시 모달 닫음
         >
+          {/* stopPropagation: 모달 내부 클릭 시 모달이 닫히는 이슈 해결해줌(모달 내부를 클릭하면 이벤트 전파를 막음) */}
           <motion.div className="relative rounded-lg bg-white p-4 shadow-lg" onClick={(e) => e.stopPropagation()}>
             <button onClick={closeModal} className="absolute right-0 top-0 m-3 text-gray-500 hover:text-gray-700">
               X
--- app/_hooks/useModal.tsx
+++ app/_hooks/useModal.tsx
@@ -1,5 +1,15 @@
 import { useState } from "react";
 
+/**
+ * 모달 상태를 관리하는 커스텀 훅
+ *
+ * @returns {Object} 모달 상태와 모달을 열고 닫는 함수들을 반환합니다.
+ * @returns {boolean} isOpen - 모달이 열려 있는지 여부를 나타내는 상태
+ * @returns {() => void} openModal - 모달을 여는 함수
+ * @returns {() => void} closeModal - 모달을 닫는 함수
+ * @author 김보미
+ */
+
 export default function useModal() {
   const [isOpen, setIsOpen] = useState(false);
 
