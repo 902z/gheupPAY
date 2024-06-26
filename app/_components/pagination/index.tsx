8ca17c52e597eeb544d9943e676d40b0097615ec
jinjo <dlawlswh123@gmail.cllm>
2024-06-26 22:39:59 +0900

    ✨ feat: 페이지 네이션 추가

--- app/_components/pagination/index.tsx
+++ app/_components/pagination/index.tsx
@@ -1,5 +1,75 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import arrowLeft from "@/public/icons/arrow-left.png";
+import arrowRight from "@/public/icons/arrow-right.png";
+import Image from "next/image";
 
-export default function index() {
-  return <div></div>;
+type PageNationProps = {
+  onChange?: (page: number) => void;
+  pageRangeDisplayed?: number;
+  totalItemsCount: number;
+  itemsCountPerPage: number;
+  activePage: number;
+  pathname: string;
+};
+
+export default function Pagination({
+  onChange,
+  pageRangeDisplayed = 7,
+  totalItemsCount,
+  itemsCountPerPage,
+  activePage,
+  pathname,
+}: PageNationProps) {
+  const nextPage =
+    Math.ceil(totalItemsCount / itemsCountPerPage) < activePage + 1
+      ? activePage
+      : activePage + 1;
+
+  const prevPage = activePage > 1 ? activePage - 1 : 1;
+
+  const totalPage = Math.ceil(totalItemsCount / itemsCountPerPage);
+  const midPoint = Math.floor(pageRangeDisplayed / 2);
+
+  const startPage = Math.max(
+    1,
+    Math.min(activePage - midPoint, totalPage - pageRangeDisplayed + 1),
+  );
+
+  const endPage = Math.min(
+    totalPage,
+    Math.max(activePage + midPoint, pageRangeDisplayed),
+  );
+
+  const paginationList = Array.from(
+    { length: endPage - startPage + 1 },
+    (_, i) => startPage + i,
+  );
+
+  return (
+    <div className="flex gap-[2px] text-m">
+      <Link
+        className="flex h-10 w-10 items-center justify-center"
+        href={{ pathname: `${pathname}`, query: { page: prevPage } }}
+      >
+        <Image src={arrowLeft} alt="arrow-left" />
+      </Link>
+      {paginationList.map((page) => (
+        <Link
+          className={`${activePage === page ? "bg-red-30 text-white" : ""} flex h-10 w-10 items-center justify-center rounded-sm`}
+          key={page}
+          href={{ pathname: `${pathname}`, query: { page: page } }}
+        >
+          {page}
+        </Link>
+      ))}
+      <Link
+        className="flex h-10 w-10 items-center justify-center"
+        href={{ pathname: `${pathname}`, query: { page: nextPage } }}
+      >
+        <Image src={arrowRight} alt="arrow-right" />
+      </Link>
+    </div>
+  );
 }
