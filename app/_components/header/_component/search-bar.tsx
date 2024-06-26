4066877a5657ca8813e88d19580d6f8cb3d997a2
codefug <86544979+codefug@users.noreply.github.com>
2024-06-26 12:30:43 +0900

    💄 ui : header 반응형 완성

--- app/_components/header/_component/search-bar.tsx
+++ app/_components/header/_component/search-bar.tsx
@@ -25,7 +25,7 @@ export default function SearchBar({ className }: SearchBarProps) {
       </section>
       <input
         type="search"
-        className="h-9 w-full rounded-[10px] bg-gray-10 px-2 py-[10px] indent-[30px] text-s md:h-10 md:max-w-[450px] md:px-[10px] md:text-m"
+        className="h-9 w-full rounded-[10px] bg-gray-10 px-2 py-[10px] indent-[30px] text-s md:h-10 md:max-w-[450px] md:px-[10px] md:text-m lg:w-full lg:max-w-[450px]"
         placeholder="가게 이름으로 찾아보세요"
         ref={inputRef}
       />
