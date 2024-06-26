bb91f033d1936f8680bcddd66ea0a8f2098f1e98
김보미 <kimbomi2172@naver.com>
2024-06-26 14:24:16 +0900

    🐛 fix: 카드 호버시 가로 스크롤 생기는 이슈 해결

--- app/(route)/(alba)/announce-list/page.tsx
+++ app/(route)/(alba)/announce-list/page.tsx
@@ -10,7 +10,7 @@ export default async function page() {
 
   return (
     <div className="mt-[102px] w-full md:mt-[70px] lg:mx-auto">
-      <div className="mb-10 bg-red-10 py-12">
+      <div className="mb-10 bg-red-10 px-4 py-12">
         <div className="mx-auto flex w-full flex-col px-2 md:justify-center lg:max-w-[1000px]">
           <h2 className="pb-4 font-bold text-l md:pb-12 md:text-2xl">
             맞춤 공고
@@ -18,7 +18,7 @@ export default async function page() {
           <CustomizedAnnounce notices={customizedNotices} />
         </div>
       </div>
-      <div className="mx-auto flex w-full flex-col px-2 md:justify-center lg:max-w-[964px]">
+      <div className="mx-auto flex w-full flex-col px-4 md:justify-center lg:max-w-[964px]">
         <h2 className="pb-4 font-bold text-l md:pb-12 md:text-2xl">
           전체 공고
         </h2>
