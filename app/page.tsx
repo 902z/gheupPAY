b8c2ec2e34be554678f028e16c9663655a306a60
BORI <88658551+Kbomi16@users.noreply.github.com>
2024-06-27 17:53:36 +0900

    ✨ feat: 모달 구현 (#52)
    * 🐛 fix: env gitignore처리
    
    * ✨ feat: 모달 기초 구현
    
    * 📝 docs: jsdoc 추가
    
    * ✨ feat: 확인 모달 구현
    
    * ♻️ refactor: 폰트 수정
    
    * ✨ feat: 경고 모달 구현
    
    * ✨ feat: 선택 모달 구현
    
    * ♻️ refactor: 버튼 크기 수정
    
    * ♻️ refactor: 폰트 굵기 설정
    
    * ♻️ refactor: 모달 내 버튼 크기 수정
    
    * ♻️ refactor: baseModal 만들어 모달들 관리하기
    
    * ♻️ refactor: 버튼 크기 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
+import Modal from "./_components/modals";
+
 export default function Home() {
   return (
     <>
-      <div className="text-center">
-        <h1 className="font-bold">폰트를 굵게 하고 싶으면 font-bold를 쓰세요.</h1>
-        <p>기본은 regular입니다. (기본은 설정 X)</p>
-        <p className="text-s">s: 12px</p>
-        <p className="text-m">m: 14px</p>
-        <p>base: 16px (기본은 설정 X)</p>
-        <p className="text-l">l: 20px</p>
-        <p className="text-xl">xl: 28px</p>
-      </div>
+      <Modal type="confirm">confirm</Modal>
+      <Modal type="warning">warning</Modal>
+      <Modal yesType="yes">select</Modal>
     </>
   );
 }
