05346a2c22bfb4ac1351ad89a2e861ab52e06a43
codefug <86544979+codefug@users.noreply.github.com>
2024-06-27 17:49:16 +0900

    ✨ feat : 최근 본 공고 저장할 전역 상태 로직 추가

--- stores/user-store.ts
+++ stores/user-store.ts
@@ -3,11 +3,33 @@ import { createJSONStorage, persist } from "zustand/middleware";
 
 type UserState = {
   type: "employer" | "employee" | null;
+  noticesData: {
+    id: number;
+    hourlyPay: number;
+    startsAt: string;
+    workhour: number;
+    description: string;
+    closed: false;
+    shop: {
+      item: {
+        id: string;
+        name: string;
+        category: string;
+        address1: string;
+        address2: string;
+        description: string;
+        imageUrl: string;
+        originalHourlyPay: number;
+      };
+      href: string;
+    };
+  }[];
 };
 
 type UserActions = {
   login: (type: "employer" | "employee") => void;
   logout: () => void;
+  postNotice: (notice: UserState["noticesData"][0]) => void;
 };
 
 export type UserStore = UserState & UserActions;
@@ -60,6 +82,25 @@ export default function Test() {
   );
 }
 
+@description
+아래 예시는 포스트를 사용하는 예시입니다! pr에 있는 영상과 일치하는 코드에요!
+@example
+
+export default function AnnounceCard({ notices }: AnnounceCardProps) {
+  const postNotice = useUserStore((state) => state.postNotice);
+
+  return (
+    <>
+      {notices &&
+        notices.map((notice) => {
+          const hourlyWage = calculateWagePercentage(notice.item.hourlyPay);
+          const date = dateFormat(notice.item.startsAt);
+          return (
+            <>
+              <button onClick={() => postNotice(notice.item)}>
+                포스트 데이터 추가
+              </button>
+
 위 예시는 store 전체를 가져와서 사용하는 예시입니다.
 단 주의할 점은 store 전체를 가져왔기 때문에 다른 곳에서 store를 변화시키면
 리턴값에 포함되든 안되든 상관없이 컴포넌트 전체 리렌더링이 일어납니다.
@@ -71,8 +112,18 @@ const useUserStore = create<UserStore>()(
   persist(
     (set, get) => ({
       type: null,
+      noticesData: [],
       login: (type: "employer" | "employee") => set({ type: type }),
       logout: () => set({ type: null }),
+      postNotice: (notice) => {
+        const notices = get().noticesData;
+        if (notices.some((n) => n.id === notice.id)) return;
+        if (notices.length >= 6) {
+          set({ noticesData: [notice, ...notices.slice(0, 5)] });
+        } else {
+          set({ noticesData: [notice, ...notices] });
+        }
+      },
     }),
     {
       name: "user-store",
