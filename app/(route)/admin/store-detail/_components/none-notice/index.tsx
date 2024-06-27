9f54846f17582eccc301b0f5a2f7726e09e0ca90
김보미 <kimbomi2172@naver.com>
2024-06-27 18:47:46 +0900

    💄 ui: 버튼 마진 수정

--- app/(route)/admin/store-detail/_components/none-notice/index.tsx
+++ app/(route)/admin/store-detail/_components/none-notice/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function NoneNotice() {
   return (
-    <div className="flex flex-col items-center justify-center rounded-xl border border-gray-20 px-40 py-10">
+    <div className="flex flex-col items-center justify-center rounded-xl border border-gray-20 px-40 py-10 md:px-60">
       <p className="my-8 text-m">공고를 등록해 보세요.</p>
       <LinkButton href="/store-register" color="orange" className="font-bold">
         공고 등록하기
