8be65d53e355d891b19d7f823beae0a66c26e348
Sparrowlim <105118884+Sparrowlim@users.noreply.github.com>
2024-06-28 13:32:09 +0900

    ✨ feat: 페이지네이션 구현 (#57)
    * 📦 chore: 이미지 추가
    
    * ✨ feat: 공고 리스트 response 타입 추가
    
    * ♻️ refector:  컴포넌트 분리
    
    * ♻️ refactor: 컴포넌트, 타입 네이밍 수정
    
    * ♻️ feat: api response 수정
    
    * ✨ feat: 페이지 네이션 추가
    
    * 💄 style: 디자인 수정
    
    * ✨ feat: 시작주소 왼쪽버튼 안눌리고 마지막 버튼일 경우 없어지게 만듬
    
    * ✏️ fix: 콘솔로그 제거
    
    * ♻️ refactor: 중복코드 제거
    
    * Marge: conflict 처리
    
    * ✨ feat: 기존 경로를 무식하게 집어 넣는 방식에서 활성화 주소를 받아 와서 처리하도록 추가
    
    * 🐛 fix: 오탈자 수정
    
    * ✨ feat: 페이지 포커스 기능 추가
    
    * ✨ feat: 텍스트 길이가 넘어서면 ...으로 표기, swiper 간격 고정으로 수정
    
    ---------
    
    Co-authored-by: jinjo <dlawlswh123@gmail.cllm>

---
+++ app/_components/notice-list/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import Pagination from "@/app/_components/pagination";
+import { NoticeResponse } from "@/app/_apis/notice/response-type.js";
+import NoticeCard from "../notice-card";
+
+type AllNoticeListProps = {
+  notices: NoticeResponse;
+  activePage: number;
+  itemsCountPerPage: number;
+};
+
+const ALL_LIST_SECTION_ID = "all-list-section";
+
+export default function AllNoticeList({
+  notices,
+  activePage,
+  itemsCountPerPage,
+}: AllNoticeListProps) {
+  const noticeList = notices.items;
+  const totalItemsCount = notices.count;
+
+  useEffect(() => {
+    if (window.location.hash) {
+      const element = document.querySelector(window.location.hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      <div className="mx-auto flex w-full flex-col px-4 md:justify-center lg:max-w-[964px]">
+        <h2
+          className="pb-4 font-bold text-l md:pb-12 md:text-2xl"
+          id={ALL_LIST_SECTION_ID}
+        >
+          전체 공고
+        </h2>
+        <div className="lg grid grid-cols-2 gap-4 lg:grid-cols-3">
+          {noticeList &&
+            noticeList.map((cardContents) => {
+              return <NoticeCard cardContents={cardContents} />;
+            })}
+        </div>
+        <div className="mb-[60px] mt-10">
+          <Pagination
+            activePage={activePage}
+            totalItemsCount={totalItemsCount}
+            itemsCountPerPage={itemsCountPerPage}
+            focusHash={ALL_LIST_SECTION_ID}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/_components/pagination/index.tsx
+++ app/_components/pagination/index.tsx
@@ -1,5 +1,101 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import arrowLeft from "@/public/icons/arrow-left.png";
+import arrowRight from "@/public/icons/arrow-right.png";
+import arrowLeftDisabled from "@/public/icons/arrow-left-disabled.png";
+import Image from "next/image";
+import { usePathname, useSearchParams } from "next/navigation";
 
-export default function index() {
-  return <div></div>;
+type PageNationProps = {
+  pageRangeDisplayed?: number;
+  totalItemsCount: number;
+  itemsCountPerPage: number;
+  activePage: number;
+  focusHash?: string;
+};
+
+export default function Pagination({
+  focusHash,
+  pageRangeDisplayed = 7,
+  totalItemsCount,
+  itemsCountPerPage,
+  activePage,
+}: PageNationProps) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const creatPageURL = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    return `${pathname}?${params.toString()}#${focusHash}`;
+  };
+
+  const totalPage = Math.ceil(totalItemsCount / itemsCountPerPage);
+
+  const nextPage = totalPage < activePage + 1 ? activePage : activePage + 1;
+
+  const prevPage = activePage > 1 ? activePage - 1 : 1;
+
+  const midPoint = Math.floor(pageRangeDisplayed / 2);
+
+  const startPage = Math.max(
+    1,
+    Math.min(activePage - midPoint, totalPage - pageRangeDisplayed + 1),
+  );
+
+  const endPage = Math.min(
+    totalPage,
+    Math.max(activePage + midPoint, pageRangeDisplayed),
+  );
+
+  const paginationList = Array.from(
+    { length: endPage - startPage + 1 },
+    (_, i) => startPage + i,
+  );
+
+  return (
+    <div className="flex justify-center gap-[2px] text-m">
+      <Link
+        className={`${activePage === 1 ? "pointer-events-none" : ""} flex h-10 w-10 items-center justify-center`}
+        href={{
+          pathname: `${pathname}`,
+          query: { page: prevPage },
+          hash: focusHash,
+        }}
+      >
+        {activePage === 1 ? (
+          <Image
+            src={arrowLeftDisabled}
+            alt="arrow-left"
+            width={20}
+            height={20}
+          />
+        ) : (
+          <Image src={arrowLeft} alt="arrow-left" width={20} height={20} />
+        )}
+      </Link>
+      {paginationList.map((page) => (
+        <Link
+          className={` ${activePage === page ? "bg-red-30 text-white" : ""} flex h-10 w-10 items-center justify-center rounded-sm`}
+          key={page}
+          href={creatPageURL(page)}
+        >
+          {page}
+        </Link>
+      ))}
+      {endPage !== totalPage ? (
+        <Link
+          className="flex h-10 w-10 items-center justify-center"
+          href={{
+            pathname: `${pathname}`,
+            query: { page: nextPage },
+            hash: focusHash,
+          }}
+        >
+          <Image src={arrowRight} alt="arrow-right" />
+        </Link>
+      ) : null}
+    </div>
+  );
 }
