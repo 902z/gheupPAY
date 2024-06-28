2575d9afd56f201d94e2e1b57feee38338d48252
김보미 <kimbomi2172@naver.com>
2024-06-28 14:49:29 +0900

    ♻️ refactor: 라벨 추가 및 카드 컴포넌트 수정

--- app/(route)/admin/notice-detail/_components/notice-detail-card/index.tsx
+++ app/(route)/admin/notice-detail/_components/notice-detail-card/index.tsx
@@ -1,5 +1,4 @@
 import { noticeDetail } from "@/app/_apis/type";
-import LabelHourlyRate from "@/app/_components/label-hourly-rate";
 import { calculateTimeRange } from "@/app/_util/calculate-time-range";
 import { calculateWagePercentage } from "@/app/_util/calculate-wage-percentage ";
 import { dateFormat } from "@/app/_util/date-format";
@@ -9,6 +8,7 @@ import React from "react";
 import clock from "@/public/icons/clock.png";
 import mapPin from "@/public/icons/map-pin.png";
 import Button from "@/app/_components/button";
+import OnlyLabelHourlyRate from "../only-label-hourly-rate";
 
 export default function NoticeDetailCard({ noticeDetail }: noticeDetail) {
   const hourlyWage = calculateWagePercentage(noticeDetail.item.hourlyPay);
@@ -23,7 +23,7 @@ export default function NoticeDetailCard({ noticeDetail }: noticeDetail) {
         </data>
       </div>
       <div className="box-border flex h-fit w-full flex-col rounded-[12px] border border-gray-10 bg-white p-5 lg:flex-row lg:gap-4">
-        <div className="relative mb-2 box-border h-[180px] w-full rounded-[12px] md:h-[160px]">
+        <div className="relative mb-2 box-border h-[180px] w-full rounded-[12px] md:h-[360px] lg:h-[308px] lg:w-[539px]">
           <Image
             alt=""
             src={noticeDetail.item.shop.item.imageUrl}
@@ -31,37 +31,42 @@ export default function NoticeDetailCard({ noticeDetail }: noticeDetail) {
             className="rounded-[12px] object-cover"
           />
         </div>
-        <div className="px-1">
-          <h3 className="font-bold text-m text-primary">식당</h3>
-          <data className="pt-1 font-bold text-ml md:text-l">
-            {formattedNumber(noticeDetail.item.hourlyPay)}원
-          </data>
-          <div className="flex items-start gap-1">
-            <Image src={clock} alt="시간" width={20} height={20} />
-            <time className="text-s text-gray-50 md:text-m">
-              {date}{" "}
-              {calculateTimeRange(
-                noticeDetail.item.startsAt,
-                noticeDetail.item.workhour,
-              )}{" "}
-              ({noticeDetail.item.workhour}시간)
-            </time>
+        <div className="relative flex-1 px-1 lg:flex lg:flex-col lg:items-stretch lg:py-3">
+          <div className="lg:flex-1">
+            <h3 className="font-bold text-m text-primary">식당</h3>
+            <div className="flex gap-2 py-2">
+              <data className="pt-1 font-bold text-ml md:text-l">
+                {formattedNumber(noticeDetail.item.hourlyPay)}원
+              </data>
+              <OnlyLabelHourlyRate percent={hourlyWage} />
+            </div>
+            <div className="flex items-start gap-1">
+              <Image src={clock} alt="시간" width={20} height={20} />
+              <time className="text-s text-gray-50 md:text-m">
+                {date}{" "}
+                {calculateTimeRange(
+                  noticeDetail.item.startsAt,
+                  noticeDetail.item.workhour,
+                )}{" "}
+                ({noticeDetail.item.workhour}시간)
+              </time>
+            </div>
+            <div className="flex items-center gap-1">
+              <Image src={mapPin} alt="장소" width={20} height={20} />
+              <data className="py-2 text-s text-gray-50 md:text-m">
+                {noticeDetail.item.shop.item.address1}
+              </data>
+            </div>
+            <div className="break-words py-4 text-m">
+              <p>{noticeDetail.item.shop.item.description}</p>
+            </div>
           </div>
-          <div className="flex items-center gap-1">
-            <Image src={mapPin} alt="장소" width={20} height={20} />
-            <data className="py-2 text-s text-gray-50 md:text-m">
-              {noticeDetail.item.shop.item.address1}
-            </data>
-          </div>
-          <div className="break-words py-4 text-m">
-            <p>{noticeDetail.item.shop.item.description}</p>
-          </div>{" "}
           <Button btnColor="white" className="font-bold">
             공고 편집하기
           </Button>
         </div>
       </div>
-      <div className="mt-4 box-border flex h-fit w-full flex-col rounded-[12px] bg-gray-10 p-5">
+      <div className="mt-4 box-border flex h-fit w-full flex-col break-words rounded-[12px] bg-gray-10 p-5">
         <h3 className="font-bold">공고 설명</h3>
         <p>{noticeDetail.item.description}</p>
       </div>
---
+++ app/(route)/admin/notice-detail/_components/only-label-hourly-rate/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+import arrowUpWhite from "@/public/icons/arrow-up-white.png";
+
+const getColorByPercentage = (percent: number) => {
+  if (percent >= 50) return "bg-red-40";
+  if (percent >= 30) return "bg-red-30";
+  if (percent >= 0) return "bg-red-20";
+};
+
+export default function OnlyLabelHourlyRate({ percent }: { percent: number }) {
+  const bgColor = getColorByPercentage(percent);
+
+  return (
+    <div
+      className={`flex items-center ${bgColor} h-[36px] w-[150px] justify-center rounded-3xl text-white`}
+    >
+      <p className="text-center font-bold text-s">기존 시급보다 {percent}%</p>
+      <Image src={arrowUpWhite} alt="" width={20} height={20} />
+    </div>
+  );
+}
