c22f9cfc021b3e7bcb6059ea601ba5b94c3ef192
BORI <88658551+Kbomi16@users.noreply.github.com>
2024-06-29 16:48:35 +0900

    ♻️ refactor: 카드 클릭시 해당 공고 페이지로 이동 (#77)
    * ♻️ refactor: siwiper 크기 조정
    
    * ♻️ refactor: swiper 마지막 여백 조정
    
    * ✨ feat: 카드 클릭시 해당 공고 페이지로 이동
    
    * ✨ feat: 카드 클릭시 해당 공고 페이지로 이동
    
    * ♻️ refactor: 알바 페이지용 버튼 수정

--- app/(route)/(alba)/notice-detail/[shop_id]/[notice_id]/page.tsx
+++ app/(route)/(alba)/notice-detail/[shop_id]/[notice_id]/page.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { getShopNoticeDetail } from "@/app/_apis/shop";
 import NoticeDetailCard from "@/app/_components/notice-detail-card";
-import RecentNotices from "../_components/recent-notices";
+import RecentNotices from "../../_components/recent-notices";
+
 export const metadata = {
   title: "공고 상세",
 };
-export default async function page() {
-  const shopId = "4490151c-5217-4157-b072-9c37b05bed47";
-  const noticeId = "99996477-82db-4bda-aae1-4044f11d9a8b";
 
-  const noticeDetail = await getShopNoticeDetail(shopId, noticeId);
+export default async function page({
+  params,
+}: {
+  params: { shop_id: string; notice_id: string };
+}) {
+  const noticeDetail = await getShopNoticeDetail(
+    params.shop_id,
+    params.notice_id,
+  );
 
   return (
     <div className="base-container">
