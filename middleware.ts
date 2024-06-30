28a8a4955fe3a2f6081eefbeafe6a91aa7a19043
codefug <86544979+codefug@users.noreply.github.com>
2024-06-30 20:03:32 +0900

    ♻️ refactor : 미들웨어 가독성을 위한 수정

--- middleware.ts
+++ middleware.ts
@@ -2,37 +2,42 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getCookie } from "./app/_util/cookie";
 
+/**
+ * 사이트의 제한 조건에 걸렸을 때 다른 곳으로 리다이렉트 시켜주는 미들웨어입니다.
+ * @author 이승현
+ * @param request
+ * @returns
+ */
 export default async function loginRequired(request: NextRequest) {
   const token = await getCookie("accessToken");
   const type = await getCookie("type");
-  if (request.nextUrl.pathname === "/") {
+  const isLandingPage = request.nextUrl.pathname === "/";
+  const isAdminPage = request.nextUrl.pathname.startsWith("/admin");
+  const isAlbaPage =
+    request.nextUrl.pathname.startsWith("/profile-detail") ||
+    request.nextUrl.pathname.startsWith("/profile-register");
+
+  if (isLandingPage) {
     return NextResponse.next();
   }
+
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-  if (type === "employee" && request.nextUrl.pathname.startsWith("/admin")) {
+
+  if (type === "employee" && isAdminPage) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-  if (
-    type === "employer" &&
-    (request.nextUrl.pathname.startsWith("/profile-detail") ||
-      request.nextUrl.pathname.startsWith("/profile-register"))
-  ) {
+  if (type === "employer" && isAlbaPage) {
     return NextResponse.redirect(new URL("/", request.url));
   }
+
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
+    // 아래 페이지들은 공통으로 들어갈 수 있는 페이지입니다.
     "/((?!login|signup|notice-list|notice-detail|api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
