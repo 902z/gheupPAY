287e8031442edd18c750c21cc9372b3e41af1f68
임진조 <dlawlswh123@gmail.com>
2024-07-01 23:13:19 +0900

    ✨ feat: shop-detail에서 shop_id 가져오는 구조로 변경, 공고 등록하기 주소 변경(아직 메인 api를 사용하지 않기 때문에 테스트 코드가 섞여 있음)

--- app/(route)/admin/shop-detail/_components/none-notice/index.tsx
+++ app/(route)/admin/shop-detail/_components/none-notice/index.tsx
@@ -1,15 +1,18 @@
+"use client";
 import { LinkButton } from "@/app/_components/button";
+import { usePathname, useSearchParams } from "next/navigation";
 import React from "react";
 
-export default function NoneNotice() {
+type NoneNoticeProps = {
+  shopId: string;
+};
+
+export default function NoneNotice({ shopId }: NoneNoticeProps) {
+  const href = `/admin/notice-create/${shopId}`;
   return (
     <div className="flex flex-col items-center justify-center rounded-xl border border-gray-20 px-40 py-10 md:px-60">
       <p className="my-8 text-m">공고를 등록해 보세요.</p>
-      <LinkButton
-        href="/admin/notice-create"
-        color="orange"
-        className="font-bold"
-      >
+      <LinkButton href={href} color="orange" className="font-bold">
         공고 등록하기
       </LinkButton>
     </div>
---
+++ app/(route)/admin/shop-detail/page.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import MyShopDetailCard from "./_components/my-shop-detail-card";
+import NoneNotice from "./_components/none-notice";
+import { getCookie } from "@/app/_util/cookie";
+import { getShopDetail, getShopInfo, getUserInfo } from "@/app/_apis/shop";
+import { ShopInfo, UserInfo } from "@/app/_apis/type";
+import { LinkButton } from "@/app/_components/button";
+
+export const metadata = {
+  title: "내 가게 상세",
+};
+
+export default async function page() {
+  const shopId = "4490151c-5217-4157-b072-9c37b05bed47";
+  const shopDetail = await getShopDetail(shopId);
+  // const userId = await getCookie("userId");
+  // if (!userId) return;
+
+  // const userInfo = await getUserInfo<UserInfo>(userId);
+  // const shopId = userInfo.item.shop?.item.id;
+  // if (shopId) {
+  //   const shopInfo = await getShopInfo<ShopInfo>(shopId);
+  //   console.log(shopInfo);
+  // }
+
+  if (!shopId) {
+    return (
+      <div className="base-container">
+        <div className="flex h-[275px] flex-col items-center justify-center">
+          {!shopId && (
+            <div className="max-w-[346px]">
+              내 가게를 소개하고 공고도 등록해 보세요{" "}
+              <LinkButton
+                href="/admin/shop-register"
+                color="orange"
+                className="mt-6 font-bold"
+              >
+                가게등록하기
+              </LinkButton>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <h2 className="pb-4 font-bold text-l md:text-2xl">내 가게</h2>
+        <MyShopDetailCard shopDetail={shopDetail} />
+        <h2 className="py-8 font-bold text-l md:text-2xl">등록한 공고</h2>
+        <NoneNotice shopId={shopId} />
+      </>
+    );
+  }
+}
