fc34678fec50a0a2b5f7b868a18ed7f8b4eb6c94
임진조 <dlawlswh123@gmail.com>
2024-07-01 23:01:04 +0900

    ✨ feat: 공고 등록을 위한 shop_id api용 타입 정의

--- app/_apis/type/index.ts
+++ app/_apis/type/index.ts
@@ -78,3 +78,86 @@ export type NoticeCardContents = {
     },
   ];
 };
+
+export type UserInfo = {
+  item: {
+    id: string;
+    email: string;
+    type: "employee" | "employer";
+    name: string;
+    phone: string;
+    address: string;
+    bio: string;
+    shop?: {
+      item: {
+        id: string;
+        name: string;
+        category: string;
+        address1: string;
+        address2: string;
+        description: string;
+        imageUrl: string;
+        originalHourlyPay: number;
+      };
+    };
+  };
+  links: [];
+};
+
+export type ShopInfo = {
+  item: {
+    id: string;
+    name: string;
+    category: string;
+    address1: string;
+    address2: string;
+    description: string;
+    imageUrl: string;
+    originalHourlyPay: number;
+    user: {
+      item: {
+        id: string;
+        email: string;
+        type: "employer" | "employee";
+        name: string;
+        phone: string;
+        address: string;
+        bio: string;
+      };
+      href: string;
+    };
+  };
+  links: [];
+};
+
+export type CreateNoticeRequest = {
+  hourlyPay: number;
+  startsAt: string;
+  workhour: number;
+  description: string;
+};
+
+export type CreateNoticeResponse = {
+  item: {
+    id: "string";
+    hourlyPay: "number";
+    startsAt: "string";
+    workhour: "number";
+    description: "string";
+    closed: "boolean";
+    shop: {
+      item: {
+        id: "string";
+        name: "string";
+        category: "string";
+        address1: "string";
+        address2: "string";
+        description: "string";
+        imageUrl: "string";
+        originalHourlyPay: "number";
+      };
+      href: "string";
+    };
+  };
+  links: [];
+};
