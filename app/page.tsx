09bd4af089526f225110e73dbcdd034f680dd204
CHAE <cowhdals1111@gmail.com>
2024-07-01 20:45:56 +0900

    🐛 Fix : 프리뷰 시도+ 코드 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,15 @@
 import Link from "next/link";
 import { FormEvent, useRef } from "react";
 import { getImageUrl } from "./_apis/image";
-
-import ShopRegisterForm from "./_components/shop-register/";
 import { NoticeCardSkeleton } from "./_components/notice-card/skeleton";
 import OpenModal from "./_components/modals";
 
 export default function Home() {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    if (!inputRef.current || !inputRef.current.files) return;
-    console.log(inputRef.current?.files?.[0]);
-    console.log(await getImageUrl(inputRef.current.files[0]));
-  };
+
   return (
     <>
       <div className="base-container flex flex-col text-center">
         <NoticeCardSkeleton />
-        <Link href="/notice-list">리스트 페이지로 이동</Link>
         <Link href="/notice-list">리스트 페이지로 이동</Link>
         <h1 className="font-bold">
           폰트를 굵게 하고 싶으면 font-bold를 쓰세요.
