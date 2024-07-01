42dc3dd70a81e3c3cfe8d70db7901e48eb4f29cf
codefug <robot9917@naver.com>
2024-07-01 18:17:28 +0900

    ♻️ refactor : 최근 본 공고 리스트 리팩토링 (최근 본 공고 리스트 관련 전역 상태 로직 변경, 타입 파일 생성) (#82)
    * 📦 chore : 인증, 공고 타입 작성
    
    * 📦 chore : api 문서에 따라서 response 타입 작성
    
    * 📦 chore : 타입 폴더에 기반해서 기존 파일의 타입 변경
    
    * ♻️ refactor : 최근 본 공고 store 방식으로 변경
    
    * ♻️ refactor : 전역 상태 관리 로직 수정 (최근 본 공고)
    
    * ♻️ refactor : 카드 컴포넌트 스켈레톤 추가
    
    * 🐛 fix : 최근 본 공고 로직 에러 수정 (중복 처리)

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { FormEvent, useRef } from "react";
 import { getImageUrl } from "./_apis/image";
 
 import ShopRegisterForm from "./_components/shop-register/";
+import { NoticeCardSkeleton } from "./_components/notice-card/skeleton";
 import OpenModal from "./_components/modals";
 
 export default function Home() {
@@ -17,6 +18,8 @@ export default function Home() {
   return (
     <>
       <div className="base-container flex flex-col text-center">
+        <NoticeCardSkeleton />
+        <Link href="/notice-list">리스트 페이지로 이동</Link>
         <Link href="/notice-list">리스트 페이지로 이동</Link>
         <h1 className="font-bold">
           폰트를 굵게 하고 싶으면 font-bold를 쓰세요.
