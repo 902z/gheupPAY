f7a8c070c21490edfdcd13239155120faa180343
김보미 <kimbomi2172@naver.com>
2024-07-01 11:54:12 +0900

    💄 ui: 테이블 신청자 목록 고정

--- app/(route)/admin/notice-detail/_components/alba-application-table/index.tsx
+++ app/(route)/admin/notice-detail/_components/alba-application-table/index.tsx
@@ -11,23 +11,32 @@ export default function AlbaApplicationTable({
 }: AlbaApplicationTableProps) {
   return (
     <>
-      <div className="relative overflow-auto rounded-xl shadow-md">
-        <table className="w-full table-fixed overflow-auto rounded-xl border border-gray-20">
+      <div className="overflow-auto rounded-xl shadow-md">
+        <table className="w-full table-auto border-collapse border-spacing-0 border-gray-20">
           <thead>
             <tr className="border bg-red-10 text-left">
-              <th className="sticky left-0 top-0 border p-4">신청자</th>
-              <th className="border p-4">소개</th>
-              <th className="border p-4">전화번호</th>
-              <th className="border p-4">상태</th>
+              <th className="sticky left-0 z-10 min-w-[80px] border bg-red-10 p-4">
+                신청자
+              </th>
+              <th className="min-w-full border bg-red-10 p-4">소개</th>
+              <th className="min-w-full border bg-red-10 p-4">전화번호</th>
+              <th className="w-max min-w-[230px] border bg-red-10 p-4">상태</th>
             </tr>
           </thead>
           <tbody>
             {applicationList.items.map((application, index) => (
               <tr key={index}>
-                <td className="border p-4">{application.item.user.name}</td>
-                <td className="border p-4">{application.item.user.bio}</td>
-                <td className="border p-4">{application.item.user.phone}</td>
-                <td className="border p-4">
+                <td className="mix-w-[200px] sticky left-0 z-10 border bg-white p-4">
+                  {application.item.user.name}test
+                </td>
+                <td className="min-w-[250px] border bg-white p-4">
+                  {application.item.user.bio}일을 꼼꼼하게 하는 성격임~~~~~~
+                  일하고싶어요
+                </td>
+                <td className="min-w-[200px] border bg-white p-4">
+                  {application.item.user.phone}010-1111-1111
+                </td>
+                <td className="border bg-white p-4">
                   <StatusLabel status={application.item.status} />
                 </td>
               </tr>
