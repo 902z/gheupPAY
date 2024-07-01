55938d816903b67727ed884e26d5bb7b0caff0b2
김보미 <kimbomi2172@naver.com>
2024-07-01 13:12:45 +0900

    ✨ feat: 신청 내역 없을 때 구현

--- app/(route)/(alba)/profile-detail/page.tsx
+++ app/(route)/(alba)/profile-detail/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import NoneApplication from "./_components/none-application";
 export const metadata = {
   title: "프로필 상세",
 };
 export default function page() {
-  return <div>profile-detail</div>;
+  return (
+    <div className="base-container">
+      <h2 className="py-8 font-bold text-l md:text-2xl">내 프로필</h2>
+      <h2 className="py-8 font-bold text-l md:text-2xl">신청 내역</h2>
+      <NoneApplication />
+    </div>
+  );
 }
