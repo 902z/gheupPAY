64e27350c3edd1695cdf023beb693c11a8a4e7d5
codefug <86544979+codefug@users.noreply.github.com>
2024-07-01 18:22:30 +0900

    📝 docs : 인피니티 스크롤 문서 추가

--- app/_components/interception-area/index.tsx
+++ app/_components/interception-area/index.tsx
@@ -5,6 +5,13 @@ interface Props {
   children: ReactNode;
 }
 
+/**
+ * @author 이승현
+ * @param onImpression 감지되면 실행할 함수
+ * @param children 감지할 영역
+ *
+ * 인피니티 스크롤을 사용할 수 있는 컴포넌트입니다. 조금이라도 보이면 onImpression 함수를 실행합니다.
+ */
 export function IntersectionArea({ onImpression, children }: Props) {
   const targetRef = useRef(null);
   useEffect(() => {
