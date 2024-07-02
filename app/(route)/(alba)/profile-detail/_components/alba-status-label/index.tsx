08a4da22b114676e6a64792f042b397395613999
김보미 <kimbomi2172@naver.com>
2024-07-02 17:24:01 +0900

    ♻️ refactor: 상태 유틸리티 타입 지정

--- app/(route)/(alba)/profile-detail/_components/alba-status-label/index.tsx
+++ app/(route)/(alba)/profile-detail/_components/alba-status-label/index.tsx
@@ -5,37 +5,29 @@ type StatusLabelProps = {
   status: GetShopsShopIdNoticesNoticeId["item"]["currentUserApplication"]["item"]["status"];
 };
 
-export default function AlbaStatusLabel({ status }: StatusLabelProps) {
-  const statusState = (status: string) => {
-    switch (status) {
-      case "pending":
-        return (
-          <div className="inline rounded-full bg-green-10 px-3 py-2 font-bold text-m text-green-20">
-            대기중
-          </div>
-        );
-      case "accepted":
-        return (
-          <div className="inline rounded-full bg-blue-10 px-3 py-2 font-bold text-m text-blue-20">
-            승인 완료
-          </div>
-        );
-      case "rejected":
-        return (
-          <div className="inline rounded-full bg-red-10 px-3 py-2 font-bold text-m text-primary">
-            거절
-          </div>
-        );
-      case "canceled":
-        return (
-          <div className="inline rounded-full bg-red-10 px-3 py-2 font-bold text-m text-primary">
-            취소
-          </div>
-        );
-      default:
-        return <div>오류</div>;
-    }
-  };
+const statusStateMap: Record<string, React.ReactNode> = {
+  pending: (
+    <div className="inline rounded-full bg-green-10 px-3 py-2 font-bold text-m text-green-20">
+      대기중
+    </div>
+  ),
+  accepted: (
+    <div className="inline rounded-full bg-blue-10 px-3 py-2 font-bold text-m text-blue-20">
+      승인 완료
+    </div>
+  ),
+  rejected: (
+    <div className="inline rounded-full bg-red-10 px-3 py-2 font-bold text-m text-primary">
+      거절
+    </div>
+  ),
+  canceled: (
+    <div className="inline rounded-full bg-red-10 px-3 py-2 font-bold text-m text-primary">
+      취소
+    </div>
+  ),
+};
 
-  return <div>{statusState(status)}</div>;
+export default function AlbaStatusLabel({ status }: StatusLabelProps) {
+  return <div>{statusStateMap[status] ?? <div>오류</div>}</div>;
 }
