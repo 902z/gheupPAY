e6a74b5bc3ee8ed061fca0e699b5bc129f73dfb3
임진조 <dlawlswh123@gmail.com>
2024-07-02 19:13:12 +0900

    ✨ feat: 프로필 등록 페이지 추가

---
+++ app/(route)/(alba)/profile-register/schema.ts
@@ -0,0 +1,11 @@
+import * as yup from "yup";
+const errorMessage = "필수로 입력해야 하는 사항입니다.";
+export const profileRegisterSchema = yup.object().shape({
+  name: yup.string().required(errorMessage),
+  phone: yup
+    .string()
+    .matches(/^010-\d{4}-\d{4}$/, "번호 형식은 010-1234-5678으로 해주세요")
+    .required(errorMessage),
+  address: yup.string().required(errorMessage),
+  bio: yup.string().required(errorMessage),
+});
