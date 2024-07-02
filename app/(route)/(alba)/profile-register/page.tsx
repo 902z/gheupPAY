b2526f1d56b3c5c4bae9221dceb85dc3633e4fed
임진조 <dlawlswh123@gmail.com>
2024-07-02 23:24:36 +0900

    💄 ui:  h1 태그명 변경

--- app/(route)/(alba)/profile-register/page.tsx
+++ app/(route)/(alba)/profile-register/page.tsx
@@ -7,7 +7,7 @@ export default function AddNotice() {
     <div className="mt-[70px]">
       <div className="ml-auto mr-auto mt-[70px] max-w-[964px] pb-20 pl-3 pr-3 pt-[60px] md:pl-8 md:pr-8">
         <div className="mb-6 flex justify-between">
-          <h1 className="font-bold text-l">공고 등록</h1>
+          <h1 className="font-bold text-l">내 프로필</h1>
         </div>
         <CreateProfileForm />
       </div>
