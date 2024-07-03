2827262cfb7bbbd49f3269ba70afc793abf43f6a
임진조 <dlawlswh123@gmail.com>
2024-07-03 16:17:12 +0900

    ✨ feat: 인터셉트 라우트 성공!

---
+++ app/(route)/(alba)/layout.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Layout({
+  children,
+  modal,
+}: {
+  children: React.ReactNode;
+  modal?: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      {modal}
+      <Link href="/profile-register">아니</Link>
+    </>
+  );
+}
