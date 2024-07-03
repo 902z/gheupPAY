c5e6f54c1dea90ee3e79c113d9616ada1103c560
hongseoha <seoseo.ha.h@gmail.com>
2024-07-04 01:15:01 +0900

    :sparkles: feat: 프로필 등록시 사용 가능한 서비스

--- app/(route)/(alba)/notice-list/page.tsx
+++ app/(route)/(alba)/notice-list/page.tsx
@@ -3,6 +3,7 @@ import CustomizedNoticeList from "./_components/customized-notice";
 import { getAllNotices, getCustomizedNotices } from "@/app/_apis/notice";
 import AllNoticeList from "@/app/_components/notice-list";
 import { getCookie } from "@/app/_util/cookie";
+import { getUser } from "@/app/_apis/user";
 import Image from "next/image";
 import bluredCustomeDesktop from "@/public/images/blured-custome-desktop.png";
 import bluredCustomeMobile from "@/public/images/blured-custome-mobile.png";
@@ -20,6 +21,7 @@ interface SearchParamsProps {
 
 export default async function page({ searchParams }: SearchParamsProps) {
   const token = await getCookie("accessToken");
+  const hasAddress = await getCookie("address");
   const customizedNotices = token ? await getCustomizedNotices({}) : null;
   const hourlyPayGte = parseInt((searchParams.wage ?? "0").replace(/,/g, ""));
   const startsAtGte =
@@ -42,7 +44,7 @@ export default async function page({ searchParams }: SearchParamsProps) {
 
   const allNoticeListClassName = keyword ? "pt-12" : "";
 
-  const bgClass = token ? "bg-red-10" : "bg-[#b3a5a2]";
+  const bgClass = token&&hasAddress ? "bg-red-10" : "bg-[#b3a5a2]";
 
   return (
     <div className="mt-[102px] w-full md:mt-[70px] lg:mx-auto">
@@ -53,7 +55,44 @@ export default async function page({ searchParams }: SearchParamsProps) {
               맞춤 공고
             </h2>
             {token ? (
-              <CustomizedNoticeList notices={customizedNotices} />
+              hasAddress ? (
+                <CustomizedNoticeList notices={customizedNotices} />
+              ) : (
+                <div className="relative">
+                  <div className="md:hidden">
+                    <Image
+                      src={bluredCustomeMobile}
+                      alt="프로필 등록시 사용 가능한 서비스입니다."
+                      priority
+                      sizes="100% 100%"
+                      className="rounded-2xl"
+                    />
+                  </div>
+                  <div className="hidden md:block">
+                    <Image
+                      src={bluredCustomeDesktop}
+                      alt="프로필 등록시 사용 가능한 서비스입니다."
+                      priority
+                      sizes="100% 100%"
+                      className="rounded-2xl"
+                    />
+                  </div>
+                  <p className="absolute bottom-[50%] right-[50%] translate-x-1/2 text-center font-bold text-l text-gray-5 md:pb-12 md:text-xl">
+                    프로필 등록 후 이용 가능한
+                    <br />
+                    사용자 맞춤 서비스입니다!
+                  </p>
+                  <div className="absolute right-[50%] top-[50%] w-60 translate-x-1/2">
+                    <LinkButton
+                      href={"/profile-register"}
+                      className="w-10 text-l"
+                      btnColor="orange"
+                    >
+                      프로필 등록 하러 가기
+                    </LinkButton>
+                  </div>
+                </div>
+              )
             ) : (
               <div className="relative">
                 <div className="md:hidden">
@@ -81,8 +120,8 @@ export default async function page({ searchParams }: SearchParamsProps) {
                 </p>
                 <div className="absolute right-[50%] top-[50%] w-60 translate-x-1/2">
                   <LinkButton
-                    href={"/admin/notice-list"}
-                    className="w-10 text-xl"
+                    href={"/login"}
+                    className="w-10 text-l"
                     btnColor="orange"
                   >
                     로그인 하러 가기
