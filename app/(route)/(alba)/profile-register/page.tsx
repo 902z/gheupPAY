a66f915f3a3ce989302265aba32614e0a1b1cd1a
임진조 <dlawlswh123@gmail.com>
2024-07-03 16:12:25 +0900

    ♻️ refactor: 폴더명 _components로 변경

--- app/(route)/(alba)/profile-register/page.tsx
+++ app/(route)/(alba)/profile-register/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import CreateProfileForm from "./components/form";
+import CreateProfileForm from "./_components/form";
 
 export default function AddNotice() {
   return (
