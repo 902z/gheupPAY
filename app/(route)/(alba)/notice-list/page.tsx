944d5b166561065ba68bf1250296562c03d1cf29
SeohaH <122506809+hongseoha@users.noreply.github.com>
2024-07-04 16:53:47 +0900

    ✨ feat: 맞춤 공고 조건에 따라 다르게 제공 (#122)
    * :sparkles: feat: 로그인 후 사용 가능한 서비스입니다.
    
    * :sparkles: feat: 로그인하러 가기 버튼
    
    * :sparkles: feat: 반응형 이미지 변경
    
    * :sparkles: feat: 프로필 등록시 사용 가능한 서비스
    
    * :package: chore: 멘트 수정
    
    * :recycle: refactor: 삼항연산자 가독성 개선

--- app/(route)/(alba)/notice-list/page.tsx
+++ app/(route)/(alba)/notice-list/page.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import CustomizedNoticeList from "./_components/customized-notice";
 import { getAllNotices, getCustomizedNotices } from "@/app/_apis/notice";
 import AllNoticeList from "@/app/_components/notice-list";
+import { getCookie } from "@/app/_util/cookie";
+import { getUser } from "@/app/_apis/user";
+import Image from "next/image";
+import bluredCustomeDesktop from "@/public/images/blured-custome-desktop.png";
+import bluredCustomeMobile from "@/public/images/blured-custome-mobile.png";
+import { LinkButton } from "@/app/_components/button";
 
 interface SearchParamsProps {
   searchParams: {
@@ -14,7 +20,9 @@ interface SearchParamsProps {
 }
 
 export default async function page({ searchParams }: SearchParamsProps) {
-  const customizedNotices = await getCustomizedNotices({});
+  const token = await getCookie("accessToken");
+  const hasAddress = await getCookie("address");
+  const customizedNotices = token ? await getCustomizedNotices({}) : null;
   const hourlyPayGte = parseInt((searchParams.wage ?? "0").replace(/,/g, ""));
   const startsAtGte =
     searchParams.startDate && new Date(searchParams.startDate).toISOString();
@@ -36,17 +44,97 @@ export default async function page({ searchParams }: SearchParamsProps) {
 
   const allNoticeListClassName = keyword ? "pt-12" : "";
 
+  const bgClass = token&&hasAddress ? "bg-red-10" : "bg-[#b3a5a2]";
+
   return (
     <div className="mt-[102px] w-full md:mt-[70px] lg:mx-auto">
       {!keyword && (
-        <div className="mb-10 bg-red-10 px-4 py-12">
+        <section className={`mb-10 ${bgClass} px-4 py-10`}>
           <div className="mx-auto flex w-full flex-col px-2 md:justify-center lg:max-w-[1000px]">
-            <h2 className="pb-4 font-bold text-l md:pb-12 md:text-2xl">
-              맞춤 공고
-            </h2>
-            <CustomizedNoticeList notices={customizedNotices} />
+            <h2 className="pb-4 font-bold text-l md:pb-8 md:text-2xl">맞춤 공고</h2>
+            {(() => {
+              if (token) {
+                if (hasAddress) {
+                  return <CustomizedNoticeList notices={customizedNotices} />;
+                } else {
+                  return (
+                    <div className="relative">
+                      <div className="md:hidden">
+                        <Image
+                          src={bluredCustomeMobile}
+                          alt="프로필 등록시 사용 가능한 서비스입니다."
+                          priority
+                          sizes="100% 100%"
+                          className="rounded-2xl"
+                        />
+                      </div>
+                      <div className="hidden md:block">
+                        <Image
+                          src={bluredCustomeDesktop}
+                          alt="프로필 등록시 사용 가능한 서비스입니다."
+                          priority
+                          sizes="100% 100%"
+                          className="rounded-2xl"
+                        />
+                      </div>
+                      <p className="absolute bottom-[50%] right-[50%] translate-x-1/2 text-center font-bold text-l text-gray-5 md:pb-12 md:text-xl">
+                        프로필 등록 후 이용 가능한
+                        <br />
+                        알바님 맞춤 서비스입니다!
+                      </p>
+                      <div className="absolute right-[50%] top-[50%] w-60 translate-x-1/2">
+                        <LinkButton
+                          href={"/profile-register"}
+                          className="w-10 text-l"
+                          btnColor="orange"
+                        >
+                          프로필 등록 하러 가기
+                        </LinkButton>
+                      </div>
+                    </div>
+                  );
+                }
+              } else {
+                return (
+                  <div className="relative">
+                    <div className="md:hidden">
+                      <Image
+                        src={bluredCustomeMobile}
+                        alt="로그인시 사용 가능한 서비스입니다."
+                        priority
+                        sizes="100% 100%"
+                        className="rounded-2xl"
+                      />
+                    </div>
+                    <div className="hidden md:block">
+                      <Image
+                        src={bluredCustomeDesktop}
+                        alt="로그인시 사용 가능한 서비스입니다."
+                        priority
+                        sizes="100% 100%"
+                        className="rounded-2xl"
+                      />
+                    </div>
+                    <p className="absolute bottom-[50%] right-[50%] translate-x-1/2 text-center font-bold text-l text-gray-5 md:pb-12 md:text-xl">
+                      로그인 후 이용 가능한
+                      <br />
+                      사용자 맞춤 서비스입니다!
+                    </p>
+                    <div className="absolute right-[50%] top-[50%] w-60 translate-x-1/2">
+                      <LinkButton
+                        href={"/login"}
+                        className="w-10 text-l"
+                        btnColor="orange"
+                      >
+                        로그인 하러 가기
+                      </LinkButton>
+                    </div>
+                  </div>
+                );
+              }
+            })()}
           </div>
-        </div>
+        </section>
       )}
       <div className={allNoticeListClassName}>
         <AllNoticeList
