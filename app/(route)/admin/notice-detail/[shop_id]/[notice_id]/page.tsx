c82bf6e1c9575111b7fc40bf7e3202279af6589f
임진조 <dlawlswh123@gmail.com>
2024-07-05 16:04:41 +0900

    🐛 fix: use client 제거

--- app/(route)/admin/notice-detail/[shop_id]/[notice_id]/page.tsx
+++ app/(route)/admin/notice-detail/[shop_id]/[notice_id]/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { Suspense } from "react";
 import NoticeDetailCard from "@/app/_components/notice-detail-card";
 import AlbaApplicationTable from "../../_components/alba-application-table";
