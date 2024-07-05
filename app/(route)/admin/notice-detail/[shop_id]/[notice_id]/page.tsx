b351e5a0094633af95bc168ea0feadb67011e481
김보미 <kimbomi2172@naver.com>
2024-07-05 20:22:28 +0900

    ♻️ refactor: 다른 사장의 공고 볼 때 신청자 못봄

--- app/(route)/admin/notice-detail/[shop_id]/[notice_id]/page.tsx
+++ app/(route)/admin/notice-detail/[shop_id]/[notice_id]/page.tsx
@@ -3,6 +3,8 @@ import NoticeDetailCard from "@/app/_components/notice-detail-card";
 import AlbaApplicationTable from "../../_components/alba-application-table";
 import { getNoticeApplications } from "@/app/_apis/application";
 import NoticeDetailCardSkeleton from "@/app/_components/notice-detail-card/_component/notice-detail-card-skeleton";
+import { getCookie } from "@/app/_util/cookie";
+import { getUser } from "@/app/_apis/user";
 
 export const metadata = {
   title: "공고 상세",
@@ -32,6 +34,16 @@ export default async function page({ searchParams, params }: PageProps) {
     limit,
   );
 
+  const userId = await getCookie("userId");
+  const type = await getCookie("type");
+
+  const userDetail = typeof userId === "string" ? await getUser(userId) : null;
+  if (!userDetail || !userDetail.item || !userDetail.item.shop) {
+    return null;
+  }
+  const isOwner =
+    type === "employer" && params.shop_id === userDetail.item.shop.item.id;
+
   return (
     <div className="base-container">
       <Suspense fallback={<NoticeDetailCardSkeleton />}>
@@ -39,11 +51,17 @@ export default async function page({ searchParams, params }: PageProps) {
       </Suspense>
       <div className="my-12">
         <h2 className="py-8 font-bold text-l md:text-2xl">신청자 목록</h2>
-        <AlbaApplicationTable
-          applicationList={applicationList}
-          activePage={page}
-          itemsCountPerPage={limit}
-        />
+        {isOwner ? (
+          <AlbaApplicationTable
+            applicationList={applicationList}
+            activePage={page}
+            itemsCountPerPage={limit}
+          />
+        ) : (
+          <div className="flex flex-col items-center justify-center rounded-xl border border-gray-20 px-10 py-10 md:px-60">
+            <p>다른 사장님 공고의 신청자 목록은 못봐요!</p>
+          </div>
+        )}
       </div>
     </div>
   );
