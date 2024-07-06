82af0ba1dff282802bdda7754ce1f8716d9646ae
CHAE <cowhdals1111@gmail.com>
2024-07-07 02:00:14 +0900

    ♻️  refactor h2->h1

--- app/(route)/admin/shop-create/layout.tsx
+++ app/(route)/admin/shop-create/layout.tsx
@@ -10,9 +10,9 @@ export default function ShopCreateLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen w-full max-w-[964px] px-3 pb-[60px] pt-[142px] md:pb-[80px] md:pt-[130px] mx-auto">
+    <div className="mx-auto min-h-screen w-full max-w-[964px] px-3 pb-[60px] pt-[142px] md:pb-[80px] md:pt-[130px]">
       <header className="h- flex w-full justify-between">
-        <h2 className="font-bold text-l leading-[25px]">가게 등록</h2>
+        <h1 className="font-bold text-l leading-[25px]">가게 등록</h1>
       </header>
       {children}
     </div>
