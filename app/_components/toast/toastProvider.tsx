15562fff407d3bbafae3a9ef1e887c1f6e08dbd9
jinjo <dlawlswh123@gmail.cllm>
2024-07-07 12:42:04 +0900

    💄 ui: 색상 바꿔봄

--- app/_components/toast/toastProvider.tsx
+++ app/_components/toast/toastProvider.tsx
@@ -9,10 +9,24 @@ interface ToastProviderProps {
 }
 
 export default function ToastProvider({ children }: ToastProviderProps) {
+  const contextClass = {
+    success: "bg-green-200",
+    error: "bg-red-40",
+    info: "bg-blue-20",
+    warning: "bg-kakao",
+    default: "bg-gray-20",
+    dark: "bg-white-600 font-gray-300",
+  };
   return (
     <>
       {children}
-      <ToastContainer />
+      <ToastContainer
+        toastClassName={(context) =>
+          contextClass[context?.type || "default"] +
+          " relative flex p-1 min-h-[46px] rounded-md justify-between overflow-hidden cursor-pointer "
+        }
+        bodyClassName={() => "text-sm font-white font-med block p-3"}
+      />
     </>
   );
 }
