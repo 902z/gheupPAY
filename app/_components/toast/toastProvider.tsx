b007598f4814c3eaf2e2a45f7a2e5f9d936ad9ec
jinjo <dlawlswh123@gmail.cllm>
2024-07-07 16:53:42 +0900

    💄 ui: 토스트 이미지 수정

--- app/_components/toast/toastProvider.tsx
+++ app/_components/toast/toastProvider.tsx
@@ -9,10 +9,24 @@ interface ToastProviderProps {
 }
 
 export default function ToastProvider({ children }: ToastProviderProps) {
+  const contextClass = {
+    success: "bg-green-20",
+    error: "bg-red-30",
+    info: "bg-blue-20",
+    warning: "bg-kakao",
+    default: "bg-gray-20",
+    dark: "bg-white-600 font-gray-300",
+  };
   return (
     <>
       {children}
-      <ToastContainer />
+      <ToastContainer
+        toastClassName={(context) =>
+          contextClass[context?.type || "default"] +
+          " relative flex min-h-5 rounded-md justify-between overflow-hidden cursor-pointer p-2 "
+        }
+        bodyClassName={() => "flex font-white block p-3 items-center"}
+      />
     </>
   );
 }
