a99b8d8c701937a579c1854cdaf174c6d72e896d
MR.J <86304360+JayChae@users.noreply.github.com>
2024-07-07 19:14:13 +0900

    💄  ui: 버튼 로딩 달리는 사람 (#178)

--- app/(route)/admin/shop-create/shop-create-form.tsx
+++ app/(route)/admin/shop-create/shop-create-form.tsx
@@ -16,7 +16,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { postShopCreate } from "@/app/_apis/shop";
 import useModal from "@/app/_hooks/use-modal";
 import ConfirmModal from "@/app/_components/modals/_components/confirm-modal";
-import pulse from "@/public/icons/pulse.svg";
+import loading from "@/public/images/loading.gif";
 
 interface FormValues {
   name: string;
@@ -187,10 +187,10 @@ function ShopCreateForm() {
         <div className="mx-auto my-0 mt-6 w-[321px] text-base md:mt-8">
           {waiting ? (
             <Image
-              src={pulse}
+              src={loading}
               alt="처리 중"
-              width={48}
-              height={48}
+              width={60}
+              height={60}
               className="mx-auto my-0"
             />
           ) : (
