702183c423b1125e9921bba486f9ef66bdd13ab3
jinjo <dlawlswh123@gmail.cllm>
2024-07-07 16:21:54 +0900

    conflict 수정

--- app/_util/get-thirty-minutes-later.ts
+++ app/_util/get-thirty-minutes-later.ts
@@ -2,9 +2,13 @@
  * 내일 날짜의 자정을 RFC 3339 형식으로 변환.
  * @returns {string} 변환된 형식의 내일 자정.
  */
-export function getTomorrowMidnight(): string {
+export function getThirtyMinutesLater(): string {
   const now = new Date();
-  const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+  const tomorrow = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() + 1,
+  );
   const year = tomorrow.getFullYear();
   const month = String(tomorrow.getMonth() + 1).padStart(2, "0");
   const day = String(tomorrow.getDate()).padStart(2, "0");
