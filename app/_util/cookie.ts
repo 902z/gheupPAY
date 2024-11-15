40b872b04093165e2e4d496472e8ca48067f1cbe
codefug <robot9917@naver.com>
2024-11-15 22:03:01 +0900

    fix: 서버 쿠키에 httpOnly 옵션 추가

--- app/_util/cookie.ts
+++ app/_util/cookie.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 
 export async function setCookie(name: string, value: string) {
   const oneDay = 24 * 60 * 60 * 1000; //일단 만료 기한은 하루로 할게요.
-  cookies().set(name, value, { expires: Date.now() + oneDay });
+  cookies().set(name, value, { expires: Date.now() + oneDay, httpOnly: true });
 }
 
 export async function deleteCookie(name: string) {
